29a4a655ca156a05534680428ea45612456f7e1a
harmy <hack4cn@gmail.com>
2012-12-31 02:37:38 +0800

    修改切图批处理，增加全部切选项

--- 1_切图.bat
+++ 1_切图.bat
@@ -9,14 +9,22 @@
 @echo 3.魔法
 @echo 4.武器
 @echo 5.角色
+@echo 6.全部
 @echo 0.转人工服务
-choice /c 123450
+choice /c 1234560
 if %ERRORLEVEL% EQU 1 set RESTYPE=npc
 if %ERRORLEVEL% EQU 2 set RESTYPE=场景
 if %ERRORLEVEL% EQU 3 set RESTYPE=魔法
 if %ERRORLEVEL% EQU 4 set RESTYPE=武器
 if %ERRORLEVEL% EQU 5 set RESTYPE=角色
-if %ERRORLEVEL% EQU 6 goto end
+if %ERRORLEVEL% EQU 6 goto all
+if %ERRORLEVEL% EQU 7 goto end
 @cutter.py %RESTYPE%
+goto end
+:all
+@cutter.py npc
+@cutter.py 场景
+@cutter.py 魔法
+@cutter.py 武器
+@cutter.py 角色
 :end
-@pause
--- exporter.py
+++ exporter.py
@@ -6,20 +6,18 @@ from helper import *
 
 def export_res(resFile, exportDir):
     force_directory(exportDir)
-    with zf.ZipFile(resFile, 'r') as zip:
-        with open(os.path.join(exportDir, 'filelist.zdat'), 'wb') as f:
-            infolist = zip.infolist()
-            f.write(struct.pack('I', len(infolist)))
+    with zf.ZipFile(resFile, 'r') as zipFile:
+        with open(os.path.join(exportDir, 'filelist.zdat'), 'wb') as filelist:
+            infolist = [info for info in zipFile.infolist() if info.CRC != 0]
+            filelist.write(struct.pack('I', len(infolist)))
             for info in infolist:
-                if info.CRC == 0:
-                    continue
-                f.write(struct.pack('50s', info.filename))
-                f.write(struct.pack('I', info.CRC))
-                f.write(struct.pack('I', info.file_size))
-                f.write(struct.pack('?', 0))
+                filelist.write(struct.pack('50s', info.filename))
+                filelist.write(struct.pack('I', info.CRC))
+                filelist.write(struct.pack('I', info.file_size))
+                filelist.write(struct.pack('?', 0))
                 print 'µ¼³ö{0}Îª{1:x}'.format(info.filename, info.CRC)
                 with open(os.path.join(exportDir, '{0:x}'.format(info.CRC)), 'wb') as exportedFile:
-                    exportedFile.write(zip.read(info))
+                    exportedFile.write(zipFile.read(info))
     compress_file(os.path.join(exportDir, 'filelist.zdat'))
 
 if __name__ == '__main__':
